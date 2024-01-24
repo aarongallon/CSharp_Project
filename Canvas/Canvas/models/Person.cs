6ed2176fc5151bc43797063a057ed909783e43f3
aarongallon <aarongallon0402@gmail.com>
2024-01-23 22:18:29 -0500

    initial commit

---
+++ Canvas/Canvas/models/Person.cs
@@ -0,0 +1,19 @@
+using System.Net.Http.Headers;
+
+namespace Canvas.Models{
+public class Person {
+
+    public int Id {get; set;}
+    public string Name { get;set;}
+
+    public Dictionary<int, double> Grades {get; set;}
+
+    public char Classification  {get; set;}
+
+    public Person(){
+        Name  = string.Empty;
+        Grades = new Dictionary<int, double>();
+    }
+}
+
+}
